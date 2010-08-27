CodeLinaro-mirror/la_kernel_experimental

[ARM] msm: secondary interrupt controller for scorpion
Signed-off-by: Brian Swetland <swetland@google.com>

arch/arm/mach-msm/irq.c
@@ -465,6 +465,13 @@ void __init msm_init_irq(void)
 		set_irq_handler(n, handle_level_irq);
 		set_irq_flags(n, IRQF_VALID);
 	}
+
+#if defined(CONFIG_ARCH_MSM_SCORPION)
+	{
+		void msm_init_sirc(void);
+		msm_init_sirc();
+	}
+#endif
 }
 
 #if defined(CONFIG_MSM_FIQ_SUPPORT)

dazed-nerd/kernel-2635

Change-Id: I5f543c834fdf79b8b8364022e9f81624ecd40d3e

arch/arm/mach-msm/irq.c
@@ -466,6 +466,13 @@ void __init msm_init_irq(void)
 		set_irq_handler(n, handle_level_irq);
 		set_irq_flags(n, IRQF_VALID);
 	}
+
+#if defined(CONFIG_ARCH_QSD8X50)
+	{
+		void msm_init_sirc(void);
+		msm_init_sirc();
+	}
+#endif
 }
 
 #if defined(CONFIG_MSM_FIQ_SUPPORT)